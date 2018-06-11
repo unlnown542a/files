73a91effa65771bf04ba0592075e8e114478751e
unlnown542a <unknown542a@i2pmail.org>
2018-06-11 04:51:00 +0300

    Huge update/refactoring strudture

--- README.md
+++ README.md
@@ -1,13 +1,52 @@
 # files
-* user.js - firefox quantum hardering (thanks to https://github.com/pyllyukko/user.js )
-* libpurple-lurch-git - build Arch package for libpurple-lurch-git, omemo in pidgin
-* pidgin - build Arch package for pidgin, libpurle, finch with minimal possible dependancies
-* privoxy.conf - minimal standalone working privoxy.conf which not break sites
 
-Pre-built packages:
-* finch-2.13.0-2-x86_64.pkg.tar.xz
-* libpurple-2.13.0-2-x86_64.pkg.tar.xz
-* libpurple-lurch-git-r94.3156e14-1-x86_64.pkg.tar.xz
-* pidgin-2.13.0-2-x86_64.pkg.tar.xz
+* user.js - firefox quantum hardering. Thanks to https://github.com/pyllyukko/user.js !
+  * ua-for-ua-switcher.txt - data for Firefox plugin - https://gitlab.com/ntninja/user-agent-switcher
 
-* x1125io.initdlight/files, containts a number of init.d startup time scripts. The application name is "Init.d Light" - https://f-droid.org/packages/x1125io.initdlight/
+* build:
+  * libpurple-lurch-git - Arch PKGBUILD for libpurple-lurch-git, omemo in pidgin
+  * pidgin - Arch PKGBUILD pidgin, libpurle, finch with minimal possible dependancies
+
+* pre-built packages:
+  * finch-2.13.0-2-x86_64.pkg.tar.xz
+  * libpurple-2.13.0-2-x86_64.pkg.tar.xz
+  * libpurple-lurch-git-r94.3156e14-1-x86_64.pkg.tar.xz
+  * pidgin-2.13.0-2-x86_64.pkg.tar.xz
+
+* android_manual_addon - ROM and data addon files which must be placed manually:
+    * some more handy binaries:
+        /system/xbin/obfs4proxy
+        /system/xbin/tor
+        /system/xbin/cryptsetup
+        /system/xbin/truecrypt
+        /system/xbin/privoxy
+        /system/xbin/encfs
+        /system/xbin/pdnsd
+        /system/xbin/i2pd
+    
+    * finetuned gps. Well, on my HTC m9 (hima) works awesome:
+        /system/etc/gps.conf
+
+    * initial firewall hook, carefully look throu it for your needs:
+        /system/bin/oem-iptables-init.sh
+
+    * micro G GSM,UMTS,LTE,WiFi NetworkLocation Center only must be installed as per author's docs, 
+      plugins like GSMLocation, WiFi.., DejaVu.. - must be installed as usual
+        /system/priv-app/Networklocation/NetworkLocation.apk
+
+    * Encdroid - create encfs encrypted folders, i placed it in /system/ so it can create encrypted folders on ext SD cards
+        /system/priv-app/Encdroid/Encdroid.apk
+
+    /data/media/0/_ETC/tor/torrc
+    /data/media/0/_ETC/tor/geoip
+    /data/media/0/_ETC/tor/DataDir
+    /data/media/0/_ETC/tor/geoip6
+    /data/media/0/_ETC/init.d/userinit
+    /data/media/0/_ETC/privoxy.conf
+    /data/media/0/_ETC/i2pd/i2pd.conf
+    /data/media/0/_ETC/i2pd/tunnels.conf
+    /data/media/0/_ETC/i2pd/enabled
+
+    * Add missing init.d support. Take here https://f-droid.org/packages/x1125io.initdlight.
+      Basically it calls "sh /sdcard/_ETC/init.d/userinit" to be more handy
+        /data/data/x1125io.initdlight/files/init_hook
---
+++ android_manual_addon/system/bin/oem-iptables-init.sh
@@ -0,0 +1,99 @@
+#!/system/bin/sh
+
+##
+## Install:
+##  mount -o remount,rw /system
+##  install -m750 -oroot -groot -p oem-iptables-init.sh /system/bin/oem-iptables-init.sh
+##  mount -o remount,ro /system
+##  reboot
+##
+
+IPTABLES=/system/bin/iptables
+IP6TABLES=/system/bin/ip6tables
+IPTABLES_OPTS='--wait'
+
+# default policy
+$IPTABLES $IPTABLES_OPTS -P INPUT DROP
+$IPTABLES $IPTABLES_OPTS -P OUTPUT DROP
+$IPTABLES $IPTABLES_OPTS -P FORWARD DROP
+$IP6TABLES $IPTABLES_OPTS -P INPUT DROP
+$IP6TABLES $IPTABLES_OPTS -P OUTPUT DROP
+$IP6TABLES $IPTABLES_OPTS -P FORWARD DROP
+$IP6TABLES $IPTABLES_OPTS -I INPUT -j REJECT
+$IP6TABLES $IPTABLES_OPTS -I OUTPUT -j REJECT
+$IP6TABLES $IPTABLES_OPTS -I FORWARD -j REJECT
+
+## FIXME: tor, i2pd, privoxy, sshd is running under root
+## SntpClient, DhcpClient
+##
+ROOT=0
+SYS_UID=1000
+MMS_UID=1001
+MMS_HOSTS=192.168.192.192/32,10.10.30.60/32
+
+# iptables tables creation, cleaning if re-run, etc...
+$IPTABLES $IPTABLES_OPTS -D INPUT  -j XX_INPUT
+$IPTABLES $IPTABLES_OPTS -D OUTPUT -j XX_OUTPUT
+$IPTABLES $IPTABLES_OPTS -N XX_INPUT
+$IPTABLES $IPTABLES_OPTS -N XX_OUTPUT 
+$IPTABLES $IPTABLES_OPTS -F XX_INPUT
+$IPTABLES $IPTABLES_OPTS -F XX_OUTPUT
+$IPTABLES $IPTABLES_OPTS -I INPUT  -j XX_INPUT
+$IPTABLES $IPTABLES_OPTS -I OUTPUT -j XX_OUTPUT
+$IPTABLES $IPTABLES_OPTS -t mangle -D FORWARD -j XX_mangle_FORWARD
+$IPTABLES $IPTABLES_OPTS -t mangle -N XX_mangle_FORWARD 
+$IPTABLES $IPTABLES_OPTS -t mangle -F XX_mangle_FORWARD 
+$IPTABLES $IPTABLES_OPTS -t mangle -I FORWARD -j XX_mangle_FORWARD
+$IPTABLES $IPTABLES_OPTS -t nat -D OUTPUT -j XX_nat_OUTPUT
+$IPTABLES $IPTABLES_OPTS -t nat -N XX_nat_OUTPUT 
+$IPTABLES $IPTABLES_OPTS -t nat -F XX_nat_OUTPUT 
+$IPTABLES $IPTABLES_OPTS -t nat -I OUTPUT -j XX_nat_OUTPUT
+$IPTABLES $IPTABLES_OPTS -D FORWARD -j XX_tether_FORWARD
+$IPTABLES $IPTABLES_OPTS -N XX_tether_FORWARD
+$IPTABLES $IPTABLES_OPTS -F XX_tether_FORWARD
+$IPTABLES $IPTABLES_OPTS -I FORWARD -j XX_tether_FORWARD
+# FORBID privoxy from ANY but lo, and ssh from Cellular facing
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  ! -i lo     -p tcp --syn -m tcp --dport 8118 -j DROP
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  ! -i lo     -p tcp --syn -m tcp --dport 8228 -j DROP
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -i rmnet_data+  -p tcp --syn -m tcp --dport 8022 -j DROP
+# Now rules..
+# I/O base rules
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -i lo -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -o lo -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -p udp -m udp --dport 9993 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -p udp -m udp --sport 9993 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -p udp -m udp --dport 9993 -j ACCEPT
+# PERMIT new DHCP
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -p udp -m udp --dport 67 -j ACCEPT
+# FIXME: i2pd, tor, privoxy, sshd - all root procs can send out. Not yet found the way to su to given user
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $ROOT    -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+# SNTP sync
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $SYS_UID -m conntrack --ctstate NEW,RELATED,ESTABLISHED -p udp -m udp --dport 123  -j ACCEPT
+# FETCH MMS
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $MMS_UID -m conntrack --ctstate NEW,RELATED,ESTABLISHED -p tcp -m tcp --dport 8080 -d $MMS_HOSTS -j ACCEPT
+# tethering USB/WIFI mode
+$IPTABLES $IPTABLES_OPTS -A XX_tether_FORWARD -i rndis0 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_tether_FORWARD -o rndis0 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_tether_FORWARD -i wlan0 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_tether_FORWARD -o wlan0 -j ACCEPT
+# REDIRECT all DNS to TOR
+$IPTABLES $IPTABLES_OPTS -t nat -A XX_nat_OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 5400
+# CLAMP MSS to PMTU
+$IPTABLES $IPTABLES_OPTS -t mangle -A XX_mangle_FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+
+## OrWall Init stub
+##
+# check if orwall iptables already initialized, do nothing
+if $IPTABLES $IPTABLES_OPTS -C ow_OUTPUT_LOCK -j DROP 2>/dev/null 
+then
+  exit 0
+else
+  #Create
+  $IPTABLES $IPTABLES_OPTS -N ow_OUTPUT_LOCK
+  $IPTABLES $IPTABLES_OPTS -N ow_INPUT_LOCK
+  $IPTABLES $IPTABLES_OPTS -A ow_OUTPUT_LOCK -j DROP
+  $IPTABLES $IPTABLES_OPTS -A ow_INPUT_LOCK  -j DROP
+fi
+## EOF
