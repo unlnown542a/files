8b69893d7b13f265e9067f0bd9a0b13425dde42e
unlnown542a <unknown542a@i2pmail.org>
2018-08-19 13:35:33 +0300

    Mega update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # files
 
+* Added fresh (2018-08-19) gsmlocation/lacells.db.bz2 for https://f-droid.org/en/packages/org.fitchfamily.android.gsmlocation/
+
 * user.js - firefox quantum hardering. Thanks to https://github.com/pyllyukko/user.js !
   * ua-for-ua-switcher.txt - data for Firefox plugin - https://gitlab.com/ntninja/user-agent-switcher
 
@@ -12,6 +14,9 @@
   * libpurple-2.13.0-2-x86_64.pkg.tar.xz
   * libpurple-lurch-git-r94.3156e14-1-x86_64.pkg.tar.xz
   * pidgin-2.13.0-2-x86_64.pkg.tar.xz
+
+* Add missing init.d support. Take here https://f-droid.org/packages/x1125io.initdlight.
+  Put symlinks to scripts into /data/data/x1125io.initdlight/files/ ...
 
 * android_manual_addon - ROM and data addon files which must be placed manually:
     * some more handy binaries:
@@ -34,20 +39,74 @@
       plugins like GSMLocation, WiFi.., DejaVu.. - must be installed as usual
       * /system/priv-app/Networklocation/NetworkLocation.apk
 
-    * Encdroid - create encfs encrypted folders, i placed it in /system/ so it can create encrypted folders on ext SD cards
-      * /system/priv-app/Encdroid/Encdroid.apk
+    * "User-Space" configs moved to "data" space /data/etc
+      ./system/xbin/obfs4proxy
+      ./system/xbin/tor
+      ./system/xbin/cryptsetup
+      ./system/xbin/truecrypt
+      ./system/xbin/privoxy
+      ./system/xbin/encfs
+      ./system/xbin/pdnsd
+      ./system/xbin/i2pd
+      ./system/bin/oem-iptables-init.sh
+      ./system/priv-app
+      ./system/priv-app/Networklocation
+      ./system/priv-app/Networklocation/NetworkLocation.apk
+      ./system/etc/gps.conf
+      ./data/tor/data
+      ./data/data/x1125io.initdlight/files/01setting
+      ./data/data/x1125io.initdlight/files/11privoxy
+      ./data/data/x1125io.initdlight/files/12sshd
+      ./data/data/x1125io.initdlight/files/00sysctl
+      ./data/data/x1125io.initdlight/files/10tor
+      ./data/bin/01setting
+      ./data/bin/11privoxy
+      ./data/bin/tdisablenetwork
+      ./data/bin/20i2pd
+      ./data/bin/tnewnym
+      ./data/bin/XTRA_STATUS
+      ./data/bin/tenablenetwork
+      ./data/bin/12sshd
+      ./data/bin/tflipflop
+      ./data/bin/00sysctl
+      ./data/bin/10tor
+      ./data/bin/XTRA_REFRESH
+      ./data/i2pd/netDb
+      ./data/i2pd/addressbook
+      ./data/i2pd/tags
+      ./data/i2pd/certificates
+      ./data/i2pd/certificates/router
+      ./data/i2pd/certificates/router/orignal_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed
+      ./data/i2pd/certificates/reseed/echelon_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed/bugme_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed/zmx_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed/lazygravy_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed/r4sas-reseed_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed/meeh_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed/creativecowpat_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed/backup_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed/atomike_at_mail.i2p.crt
+      ./data/i2pd/certificates/reseed/igor_at_novg.net.crt
+      ./data/i2pd/certificates/family
+      ./data/i2pd/certificates/family/gostcoin.crt
+      ./data/i2pd/certificates/family/mca2-i2p.crt
+      ./data/i2pd/certificates/family/aloneinthedark.crt
+      ./data/i2pd/certificates/family/i2pd-dev.crt
+      ./data/i2pd/certificates/family/i2p-dev.crt
+      ./data/i2pd/certificates/family/volatile.crt
+      ./data/i2pd/destinations
+      ./data/i2pd/peerProfiles
+      ./data/i2pd/family
+      ./data/etc/torrc
+      ./data/etc/sysctl.conf
+      ./data/etc/geoip
+      ./data/etc/geoip6
+      ./data/etc/privoxy.conf
+      ./data/etc/i2pd
+      ./data/etc/i2pd/i2pd.conf
+      ./data/etc/i2pd/tunnels.conf
 
-    * "User-Space", "sdcard-space" init.d and tor,i2pd,privoxy configs. All of the as follow KISS philosophy
-      * /data/media/0/_ETC/tor/torrc
-      * /data/media/0/_ETC/tor/geoip
-      * /data/media/0/_ETC/tor/DataDir
-      * /data/media/0/_ETC/tor/geoip6
-      * /data/media/0/_ETC/init.d/userinit
-      * /data/media/0/_ETC/privoxy.conf
-      * /data/media/0/_ETC/i2pd/i2pd.conf
-      * /data/media/0/_ETC/i2pd/tunnels.conf
-      * /data/media/0/_ETC/i2pd/enabled
 
-    * Add missing init.d support. Take here https://f-droid.org/packages/x1125io.initdlight.
-      Basically it calls "sh /sdcard/_ETC/init.d/userinit" to be more simple
-      * /data/data/x1125io.initdlight/files/init_hook
+
+
--- android_manual_addon/system/bin/oem-iptables-init.sh
+++ android_manual_addon/system/bin/oem-iptables-init.sh
@@ -11,6 +11,7 @@
 IPTABLES=/system/bin/iptables
 IP6TABLES=/system/bin/ip6tables
 IPTABLES_OPTS='--wait'
+IP6TABLES_OPTS='--wait'
 
 # default policy
 $IPTABLES $IPTABLES_OPTS -P INPUT DROP
@@ -21,67 +22,90 @@ $IP6TABLES $IPTABLES_OPTS -P OUTPUT DROP
 $IP6TABLES $IPTABLES_OPTS -P FORWARD DROP
 $IP6TABLES $IPTABLES_OPTS -I INPUT -j REJECT
 $IP6TABLES $IPTABLES_OPTS -I OUTPUT -j REJECT
-$IP6TABLES $IPTABLES_OPTS -I FORWARD -j REJECT
 
 ## FIXME: tor, i2pd, privoxy, sshd is running under root
 ## SntpClient, DhcpClient
-##
+
 ROOT=0
-SYS_UID=1000
-MMS_UID=1001
-MMS_HOSTS=192.168.192.192/32,10.10.30.60/32
+WIREGUARD=10085
+SYSTEM=1000
+LINPHONE=10339
+MMSSRV=1001
+
+# mms proxy port can be found in sim card APN settings, usually 8080
+MMSPROXY=192.168.192.192/32,10.10.30.60/32
+
+# port is 80, since those are http servers
+XTRAHOST=13.33.0.0/16,54.192.0.0/16,52.85.0.0/16,13.32.0.0/16,54.230.0.0/16,52.222.0.0/16
+
 
 # iptables tables creation, cleaning if re-run, etc...
 $IPTABLES $IPTABLES_OPTS -D INPUT  -j XX_INPUT
 $IPTABLES $IPTABLES_OPTS -D OUTPUT -j XX_OUTPUT
+$IPTABLES $IPTABLES_OPTS -D FORWARD -j XX_TETHER_FORWARD
 $IPTABLES $IPTABLES_OPTS -N XX_INPUT
 $IPTABLES $IPTABLES_OPTS -N XX_OUTPUT 
+$IPTABLES $IPTABLES_OPTS -N XX_TETHER_FORWARD
 $IPTABLES $IPTABLES_OPTS -F XX_INPUT
 $IPTABLES $IPTABLES_OPTS -F XX_OUTPUT
+$IPTABLES $IPTABLES_OPTS -F XX_TETHER_FORWARD
 $IPTABLES $IPTABLES_OPTS -I INPUT  -j XX_INPUT
 $IPTABLES $IPTABLES_OPTS -I OUTPUT -j XX_OUTPUT
-$IPTABLES $IPTABLES_OPTS -t mangle -D FORWARD -j XX_mangle_FORWARD
-$IPTABLES $IPTABLES_OPTS -t mangle -N XX_mangle_FORWARD 
-$IPTABLES $IPTABLES_OPTS -t mangle -F XX_mangle_FORWARD 
-$IPTABLES $IPTABLES_OPTS -t mangle -I FORWARD -j XX_mangle_FORWARD
-$IPTABLES $IPTABLES_OPTS -t nat -D OUTPUT -j XX_nat_OUTPUT
-$IPTABLES $IPTABLES_OPTS -t nat -N XX_nat_OUTPUT 
-$IPTABLES $IPTABLES_OPTS -t nat -F XX_nat_OUTPUT 
-$IPTABLES $IPTABLES_OPTS -t nat -I OUTPUT -j XX_nat_OUTPUT
-$IPTABLES $IPTABLES_OPTS -D FORWARD -j XX_tether_FORWARD
-$IPTABLES $IPTABLES_OPTS -N XX_tether_FORWARD
-$IPTABLES $IPTABLES_OPTS -F XX_tether_FORWARD
-$IPTABLES $IPTABLES_OPTS -I FORWARD -j XX_tether_FORWARD
-# FORBID privoxy from ANY but lo, and ssh from Cellular facing
-$IPTABLES $IPTABLES_OPTS -A XX_INPUT  ! -i lo     -p tcp --syn -m tcp --dport 8118 -j DROP
-$IPTABLES $IPTABLES_OPTS -A XX_INPUT  ! -i lo     -p tcp --syn -m tcp --dport 8228 -j DROP
-$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -i rmnet_data+  -p tcp --syn -m tcp --dport 8022 -j DROP
-# Now rules..
-# I/O base rules
-$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -i lo -j ACCEPT
-$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -o lo -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -I FORWARD -j XX_TETHER_FORWARD
+$IPTABLES $IPTABLES_OPTS -t nat -D OUTPUT -j XX_OUTPUT
+$IPTABLES $IPTABLES_OPTS -t nat -N XX_OUTPUT 
+$IPTABLES $IPTABLES_OPTS -t nat -F XX_OUTPUT 
+$IPTABLES $IPTABLES_OPTS -t nat -I OUTPUT -j XX_OUTPUT
+$IPTABLES $IPTABLES_OPTS -t mangle -D FORWARD -j XX_MANGLE_FORWARD
+$IPTABLES $IPTABLES_OPTS -t mangle -N XX_MANGLE_FORWARD 
+$IPTABLES $IPTABLES_OPTS -t mangle -F XX_MANGLE_FORWARD 
+$IPTABLES $IPTABLES_OPTS -t mangle -I FORWARD -j XX_MANGLE_FORWARD
+
+# INPUT BASE RULES
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -i rmnet_data+ -p tcp --syn -m tcp --dport 8022 -j DROP
 $IPTABLES $IPTABLES_OPTS -A XX_INPUT  -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
-$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -p udp -m udp --dport 9993 -j ACCEPT
-$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -p udp -m udp --sport 9993 -j ACCEPT
-$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -p udp -m udp --dport 9993 -j ACCEPT
-# PERMIT new DHCP
-$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -p udp -m udp --dport 67 -j ACCEPT
-# FIXME: i2pd, tor, privoxy, sshd - all root procs can send out. Not yet found the way to su to given user
-$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $ROOT    -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
-# SNTP sync
-$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $SYS_UID -m conntrack --ctstate NEW,RELATED,ESTABLISHED -p udp -m udp --dport 123  -j ACCEPT
-# FETCH MMS
-$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $MMS_UID -m conntrack --ctstate NEW,RELATED,ESTABLISHED -p tcp -m tcp --dport 8080 -d $MMS_HOSTS -j ACCEPT
-# tethering USB/WIFI mode
-$IPTABLES $IPTABLES_OPTS -A XX_tether_FORWARD -i rndis0 -j ACCEPT
-$IPTABLES $IPTABLES_OPTS -A XX_tether_FORWARD -o rndis0 -j ACCEPT
-$IPTABLES $IPTABLES_OPTS -A XX_tether_FORWARD -i wlan0 -j ACCEPT
-$IPTABLES $IPTABLES_OPTS -A XX_tether_FORWARD -o wlan0 -j ACCEPT
-# REDIRECT all DNS to TOR
-$IPTABLES $IPTABLES_OPTS -t nat -A XX_nat_OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 5400
+# OUTPUT BASE RULES
+#$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+# USB/WIFI TETHER mode
+$IPTABLES $IPTABLES_OPTS -A XX_TETHER_FORWARD -i rndis0 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_TETHER_FORWARD -o rndis0 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_TETHER_FORWARD -i wlan0 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_TETHER_FORWARD -o wlan0 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -o wlan0  -p udp -m udp --sport 53 -d 192.168.0.0/16 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -o rndis0 -p udp -m udp --sport 53 -d 192.168.0.0/16 -j ACCEPT
+
+# nat TETHER - RETURN source port reply 53
+$IPTABLES $IPTABLES_OPTS -t nat -A XX_OUTPUT -p udp -m udp --sport 53 -j RETURN
+$IPTABLES $IPTABLES_OPTS -t nat -A XX_OUTPUT -p udp -m udp --sport 53 -j RETURN
+
+# I2PD and TOR
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -m owner --uid-owner $ROOT -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $ROOT -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+
+# WIREGUARD
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -m owner --uid-owner $WIREGUARD -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $WIREGUARD -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+
+# SIP: linphone
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -d 127.0.0.1/32 -m owner --uid-owner $LINPHONE -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -p udp          -m owner --uid-owner $LINPHONE -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+#$IPTABLES $IPTABLES_OPTS -t nat -A XX_OUTPUT -p tcp   -m owner --uid-owner $LINPHONE -j REDIRECT --to-ports 9040
+$IPTABLES $IPTABLES_OPTS -t nat  -A XX_OUTPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m owner --uid-owner $LINPHONE -m comment --comment "Force Linphone through TransPort" -j REDIRECT --to-ports 9040
+
+# DHCP
+$IPTABLES $IPTABLES_OPTS -A XX_INPUT  -p udp -m udp --sport 67:68 --dport 67:68 -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -p udp -m udp --sport 67:68 --dport 67:68 -j ACCEPT
+
+# NTP(SNTP), MMS, XTRA(GPS)
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $SYSTEM -m conntrack --ctstate NEW,ESTABLISHED -p udp -m udp --dport 123  -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $SYSTEM -m conntrack --ctstate NEW,ESTABLISHED -p tcp -m tcp --dport 80   -d $XTRAHOST -j ACCEPT
+$IPTABLES $IPTABLES_OPTS -A XX_OUTPUT -m owner --uid-owner $MMSSRV -m conntrack --ctstate NEW,ESTABLISHED -p tcp -m tcp --dport 8080 -d $MMSPROXY -j ACCEPT
+
+# REDIRECT to TOR
+$IPTABLES $IPTABLES_OPTS -t nat -A XX_OUTPUT -p udp -m udp --dport 53 -j REDIRECT --to-ports 5400
+
 # CLAMP MSS to PMTU
-$IPTABLES $IPTABLES_OPTS -t mangle -A XX_mangle_FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+$IPTABLES $IPTABLES_OPTS -t mangle -A XX_MANGLE_FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
 ## OrWall Init stub
 ##
